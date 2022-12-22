199e810c384c884cd632ad768effcf658e55b176
Victoria <si4yova2010@yandex.ru>
2022-12-22 18:47:24 +0500

    task № 1

---
+++ home_work_first/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите первое число: ");
+int num_A = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int num_B = Convert.ToInt32(Console.ReadLine());
+
+if (num_A > num_B)
+{
+    Console.WriteLine("max = " + num_A + " " + "min = " + num_B);
+}
+else
+{
+    Console.WriteLine("max = " + num_B + " " + "min = " + num_A);
+}
+            
+
