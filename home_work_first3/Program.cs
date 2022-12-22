9c02fd5c40eae2f9cf82a3692c9cb42cbd111c1f
Victoria <si4yova2010@yandex.ru>
2022-12-22 18:55:42 +0500

    task № 3

---
+++ home_work_first3/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число:");
+num = Convert.ToInt32(Console.ReadLine());
+
+if (num % 2 == 1)
+{
+    Console.WriteLine("Число " + num + " является: НЕЧЁТНЫМ");
+}
+else
+{
+    Console.WriteLine("Число " + num + "является: ЧЁТНЫМ");
+}
