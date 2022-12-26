1c65dab5af1490afb52549aa24f616fbbbda9831
Victoria <si4yova2010@yandex.ru>
2022-12-26 18:43:10 +0500

    add task 1

---
+++ home_work_second/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите трехзначное число.\nВыхода из программы команда Exit.");
+    while(true)
+    {
+        string input = Console.ReadLine();
+        if(!input.Equals("exit"))
+            Console.WriteLine("{0}->{1}",input, input[1]);
+        else
+            break;
+    }
