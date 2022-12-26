a40bb46c026cbcfda2bf555f8cc0dcd8dff545e7
Victoria <si4yova2010@yandex.ru>
2022-12-26 19:37:26 +0500

    homework

--- home_work_first3/Program.cs
+++ home_work_first3/Program.cs
@@ -1,12 +1,12 @@
 ﻿// Напишите программу, которая на вход принимает число и выдает, является ли число четным или нет
 Console.WriteLine("Введите число:");
-num = Convert.ToInt32(Console.ReadLine());
+num1 = Convert.ToInt32(Console.ReadLine());
 
-if (num % 2 == 1)
+if (num1 % 2 == 1)
 {
-    Console.WriteLine("Число " + num + " является: НЕЧЁТНЫМ");
+    Console.WriteLine("Число " + num1 + " является: НЕЧЁТНЫМ");
 }
 else
 {
-    Console.WriteLine("Число " + num + "является: ЧЁТНЫМ");
+    Console.WriteLine("Число " + num1 + "является: ЧЁТНЫМ");
 }
