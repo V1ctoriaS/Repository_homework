38428764650f6d9164ed9270a58ba9b8af12776d
Victoria <si4yova2010@yandex.ru>
2022-12-26 19:02:09 +0500

    add task 3

---
+++ home_work_second3/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+{
+    Console.WriteLine ("Введите цифру от 1 до 7");
+    int dayofweek = int.Parse (Console.ReadLine());
+    if (dayofweek == 6 || dayofweek == 7) Console.WriteLine ("Ответ: Выходной день.");
+    else if (dayofweek <= 5) Console.WriteLine ("Ответ: Будний день.");
+    else if (dayofweek > 7) Console.WriteLine ("Ошибка: В неделе всего 7 дней!");
+}
