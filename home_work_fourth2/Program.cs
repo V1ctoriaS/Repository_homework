73496ffe5ecdbd3a8f4b70e25fe9c77f5cce79c2
Victoria <si4yova2010@yandex.ru>
2023-01-19 20:55:20 +0500

    task 27

---
+++ home_work_fourth2/Program.cs
@@ -0,0 +1,35 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе
+
+int number = getUserData("Введите число: ");
+int len = getNumberOfDigit(number);
+SumNumbers(number, len);
+
+int getUserData(string message)
+{
+    Console.WriteLine(message);
+    int userData = int.Parse(Console.ReadLine()!);
+    return userData;
+}
+
+int getNumberOfDigit(int number)
+{
+    int NumberOfDigit = 0;
+    while (number > 0)
+    {
+        number = number / 10;
+        NumberOfDigit++;
+    }
+    return NumberOfDigit;
+}
+
+
+void SumNumbers(int n, int len)
+{
+    int sum = 0;
+    for (int i = 1; i <= len; i++)
+    {
+        sum += n % 10;
+        n /= 10;
+    }
+    Console.WriteLine($"Сумма цифр = {sum}");
+}
