00aaa4519ebd465470769ffb792223e9cba5c381
Victoria <si4yova2010@yandex.ru>
2023-01-23 22:46:23 +0500

    task 38

---
+++ home_work_fifth2/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным 
+//элементов массива.
+
+double[] generateArray(int length, int start, int end)
+{
+    double[] result = new double[length];
+    for (int i = 0; i < length; i++)
+    {
+        result[i] = new Random().Next(start, end + 1);  
+    }
+    return result;
+}
+
+void printArray(double[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] );
+        if (i < array.Length - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.WriteLine("]");
+}
+
+double[] array = generateArray(10,1,100); 
+printArray(array);
+
+double min = Int32.MaxValue;
+double max = Int32.MinValue;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > max)
+        {
+            max = array[i];
+        }
+    if (array[i] < min)
+        {
+            min = array[i];
+        }
+}
+Console.WriteLine($"Разница между максимальным {max} и минимальным {min} элементами = {max - min}");
+
